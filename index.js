886ece0e4a3b31ec2409af8c4bfabd2a9a14d488
gabriel-trujillo-globant <joaquin.trujillo+github@globant.com>
2022-10-06 16:38:49 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello, Node.js v${process.versions.node}!`);
+function Animal(type) {
+  this.type = type;
+}
+
+const ani1 = new Animal('spider');
+const ani2 = Animal('wolf');
+const ani3 = Object.create(/* What's needed here to create an instance of Animal? */);
+
+console.log({ ani1, ani2, ani3 });  // What's the output?
