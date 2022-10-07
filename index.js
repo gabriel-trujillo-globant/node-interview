43fdf13f9e4e248f12b3d7d98c6b1a13afc18afe
gabriel-trujillo-globant <joaquin.trujillo+github@globant.com>
2022-10-07 11:19:10 -0500

    1 file modified

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
   setTimeout(function(){console.log('Timeout 1000')}, 1000);
   new Promise(function (res){res('Promise Then')}).then(console.log);
   setImmediate(function(){console.log('Immediate')});
+  new Promise(function(res){setTimeout(function(){console.log('Promise Timeout 1000')}, 1000)});
   process.nextTick(function(){console.log('Next tick')});
   setTimeout(function(){console.log('Timeout 0')}, 0); 
   Promise.resolve('Promise Resolve').then(console.log);
